d9d9728b361d0de86d68f8927785a3d6397e909d
shmuro <sh.muro@gmail.com>
2013-10-26 23:40:46 +0300

    added map

--- war/WEB-INF/views/googlemap.jsp
+++ war/WEB-INF/views/googlemap.jsp
@@ -4,15 +4,48 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3&sensor=false&key=AIzaSyAW42eckhtmysu0PMPYY_zv3EP2jw_Ps5w"></script>
-    <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markermanager/src/markermanager.js"></script>
-    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="/js/map.js" type="text/javascript"></script>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<style type="text/css">
+html {
+	height: 100%
+}
+
+body {
+	height: 100%;
+	margin: 0;
+	padding: 0
+}
+
+#map_canvas {
+	border: 1px black solid;
+	height: 80%;
+	margin: 10px;
+	box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.7);
+}
+
+.row {
+	display: inline-block;
+}
+</style>
+<script type="text/javascript"
+	src="https://maps.googleapis.com/maps/api/js?v=3&language=uk&sensor=false&key=AIzaSyAW42eckhtmysu0PMPYY_zv3EP2jw_Ps5w"></script>
+<!--  <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markermanager/src/markermanager.js"></script>-->
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="/js/map.js" type="text/javascript"></script>
+<script src="/js/markermanager.js" type="text/javascript"></script>
+<script src="/js/smartinfowindow.js" type="text/javascript"></script>
 <title>Google map</title>
 </head>
 <body>
-	<h1>${message}</h1>
-    <div id="map_canvas" style="height: 400px;"></div>
-    <button type="button" id="click">Click Me!</button>
+	<%-- <h1>${message}</h1> --%>
+	<div class="row">
+		<button type="button" id="cleanAll" class>Clean</button>
+		<button type="button" id="addMarker">addMarker</button>
+		<button type="button" id="center">Center</button>
+		zoom&nbsp;-&nbsp;
+		<div id="zoom"></div>
+		 lacation&nbsp;-&nbsp;<div id="lacation"></div>
+	</div>
+	<div id="map_canvas"></div>
 </body>
 </html>
--- war/js/map.js
+++ war/js/map.js
@@ -1,31 +1,121 @@
 var map;
 var mgr;
+var geocoder;
+function createMarker(title) {
+
+	var latLngPosn = new google.maps.LatLng(map.getCenter().lat(), map
+			.getCenter().lng());
+
+	var iconImage = new google.maps.MarkerImage(
+			'http://chart.apis.google.com/chart?cht=mm&chs=32x32&chco=FFFF00FF,FFFF00FF,000000FF&ext=.png',
+			new google.maps.Size(24, 14), new google.maps.Point(0, 0),
+			new google.maps.Point(0, 32));
+
+	var iconShadow = new google.maps.MarkerImage(
+			'http://chart.apis.google.com/chart?cht=mm&chs=32x32&chco=FF0000FF,FF0000FF,000000FF&ext=.png',
+			new google.maps.Size(40, 30), new google.maps.Point(0, 0),
+			new google.maps.Point(0, 32));
+
+	var iconShape = {
+		coord : [ 1, 1, 1, 32, 32, 32, 32, 1 ],
+		type : 'poly'
+	};
+
+	var markerOptions = {
+		position : latLngPosn,
+		title : title,
+		shadow : iconImage,
+		icon : iconShadow,
+		animation : google.maps.Animation.DROP, /* google.maps.Animation.BOUNCE, */
+		shape : iconShape
+	};
+
+	var marker = new google.maps.Marker(markerOptions);
+
+	google.maps.event.addListener(marker, 'dblclick', function() {
+		mgr.removeMarker(marker)
+	});
+
+	google.maps.event.addListener(marker, 'click', function(e) {
+		geocodePosition(marker.getPosition());
+	});
+	return marker;
+}
 
 function initialize() {
+
+	geocoder = new google.maps.Geocoder();
+
+	var latLng = new google.maps.LatLng(49.854808, 24.030426);
+
 	var myOptions = {
 		zoom : 10,
-		center : new google.maps.LatLng(49.854808, 24.030426),
+		center : latLng,
+		maxZoom : 20,
+		minZoom : 1,
 		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map($('#map_canvas')[0], myOptions);
 	mgr = new MarkerManager(map, myOptions);
-	var marker = new google.maps.Marker({
-		position : new google.maps.LatLng(49.854808, 24.030426),
-		animation : google.maps.Animation.DROP,
-		title : "Random marker #"
+
+	// google.maps.event.addListener(mgr, 'loaded', function() {
+	// mgr.addMarker(marker, 0);
+	// mgr.refresh();
+	// });
+
+	google.maps.event.addListener(map, 'zoom_changed', function() {
+		$('#zoom').html(map.getZoom());
 	});
 
-	google.maps.event.addListener(mgr, 'loaded', function() {
-		mgr.addMarker(marker, 0);
-		mgr.refresh();
+	google.maps.event.addDomListener($('#center')[0], 'click', function() {
+		map.panTo(latLng);
 	});
 
-	google.maps.event.addDomListener($('#click')[0], 'click', function() {
+	google.maps.event.addDomListener($('#cleanAll')[0], 'click', function() {
 		mgr.clearMarkers()
 	});
 
-	google.maps.event.addDomListener(window, 'load', initialize);
+	google.maps.event.addDomListener($('#addMarker')[0], 'click', function() {
+		var marker = createMarker("center");
+		mgr.addMarker(createMarker("center"), map.getZoom());
+		mgr.refresh();
+	});
 }
+
+function geocodePosition(pos) {
+	var content = '';
+	geocoder.geocode({
+		latLng : pos
+	}, function(responses) {
+		if (responses && responses.length > 0) {
+			for ( var i in responses) {
+				if (responses.hasOwnProperty(i)) {
+					content += responses[i].formatted_address + '</br> ';
+				}
+			}
+		} else {
+			content = 'Cannot determine address at this location.';
+		}
+
+		var infobox = new SmartInfoWindow({
+			position : pos,
+			map : map,
+			content : '<div style="text-align: center; font-size:14px;">'
+					+ content + '</div>'
+		});
+	});
+}
+
+// <script type="text/javascript">google.load("jquery", "1");</script>
+// <script src="http://www.google.com/jsapi"></script>
+// function loadMap(langCode) {
+// google.load('maps', '3.7', {
+// 'other_params' :
+// 'sensor=false&libraries=places&language=' + langCode,
+// 'callback' : mapsLoaded
+// });
+// }
+
 $(document).ready(function() {
-	initialize();
+	google.maps.event.addDomListener(window, 'load', initialize);
 });
